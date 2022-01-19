44ec3607ae54337b431da3a6938c51cafa7d6ed5
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-19 09:45:31 -0500

    Forget Password Module

--- Proyecto/Codigo/mzmotos/src/app/pages/login/login.component.ts
+++ Proyecto/Codigo/mzmotos/src/app/pages/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from 'src/lib/auth.service';
 import { UserService } from 'src/app/services/user.service';
 import { decode } from 'src/lib/token';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -19,19 +21,28 @@ export class LoginComponent implements OnInit {
     keepLog: false,
   }
 
-  private cookieName: string = "logged-user";
-  private cookieRole: string = "role";
+  forgetPasswordInput = {
+    username: "",
+    newPassword: "",
+    confirmPassword: "",
+  }
+
+  stepCounter: number = 1;
+
+  @ViewChild("forgetPassword") forgetPassword: ElementRef;
 
   constructor(
     private userService: UserService,
     private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private modalService: NgbModal,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   onChange(value: any) {
-    if(value.type === 'checkbox') {
+    if (value.type === 'checkbox') {
       this.input = { ... this.input, [value.name]: value.checked };
     } else {
       this.input = { ... this.input, [value.name]: value.value };
@@ -45,6 +56,81 @@ export class LoginComponent implements OnInit {
         const token = decode(res);
         this.router.navigate([`/${token.role}`]);
       })
+  }
+
+  increaseStep() {
+    this.stepCounter++;
+  }
+
+  decreaseStep() {
+    this.stepCounter--;
+    if (this.stepCounter < 1) {
+      this.stepCounter = 1;
+    }
+  }
+
+  onSubmitForgetPassword(forgetPasswordForm: NgForm) {
+    console.log(this.forgetPasswordInput);
+    if (this.stepCounter === 1) {
+      this.forgetPasswordInput.username = forgetPasswordForm.value.username;
+      this.checkUserExist(this.forgetPasswordInput.username);
+    } else if (this.stepCounter === 2) {
+      this.forgetPasswordInput.newPassword = forgetPasswordForm.value.password;
+      this.forgetPasswordInput.confirmPassword = forgetPasswordForm.value.confirm;
+      if (!this.checkPassword(this.forgetPasswordInput.newPassword, this.forgetPasswordInput.confirmPassword)) {
+        return;
+      }
+      this.userService.updatePassword(this.forgetPasswordInput.username, this.forgetPasswordInput.newPassword)
+        .subscribe((res: any) => {
+          this.toast.success("Password updated", "Success",
+            {
+              timeOut: 3000,
+            });
+          this.modalClose();
+        });
+    }
+  }
+
+  checkUserExist(username: string) {
+    this.userService.getUserByUsername(username)
+      .subscribe((res: any) => {
+        if (!res._id) {
+          this.toast.error("User not found", "Error", {
+            timeOut: 3000,
+          });
+          this.stepCounter = 1;
+        } else {
+          this.increaseStep();
+        }
+      });
+  }
+
+  checkPassword(password: string, confirmPassword: string) {
+    if (password !== confirmPassword) {
+      this.toast.error("Password don't match", "Error", {
+        timeOut: 3000,
+      });
+      return false;
+    }
+    if (password.length < 6) {
+      this.toast.error("Too short password", "Error", {
+        timeOut: 3000,
+      });
+      return false;
+    }
+    return true;
+  }
+
+  showForgetPassword() {
+    this.triggerModal(this.forgetPassword);
+  }
+
+  triggerModal(content: any) {
+    this.modalService.open(content).result;
+  }
+
+  modalClose() {
+    this.modalService.dismissAll();
   }
 
 }
--- Proyecto/Codigo/mzmotos/src/app/services/user.service.ts
+++ Proyecto/Codigo/mzmotos/src/app/services/user.service.ts
@@ -18,18 +18,14 @@ export class UserService {
   }
 
   signin(username: string, password: string) {
-    return this.http.post(`${CONSTANTS.API_URL}/users/signin`, {username, password});
+    return this.http.post(`${CONSTANTS.API_URL}/users/signin`, {username: username, password: password});
   }
 
   updatePassword(username:string, password: string) {
     return this.http.put(`${CONSTANTS.API_URL}/users/password/${username}`, {username, password});
   }
 
-  getUserByID(id: string) {
-    return this.http.get(`${CONSTANTS.API_URL}/user/users/${id}`);
-  }
-
-  deleteUserByID(id: string) {
-    return this.http.delete(`${CONSTANTS.API_URL}/user/users/${id}`);
+  getUserByUsername(username: string) {
+    return this.http.get(`${CONSTANTS.API_URL}/users/username/${username}`);
   }
 }
