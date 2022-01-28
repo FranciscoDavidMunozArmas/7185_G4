e43c13c1869acf37e3361cbe08589886d6019a00
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-28 16:32:14 -0500

    Update user, manager, salesman and warehouse

--- Proyecto/Codigo/mzmotos/src/app/models/Client.ts
+++ Proyecto/Codigo/mzmotos/src/app/models/Client.ts
@@ -2,12 +2,14 @@ export class Client {
     _id?: string;
     RUC: string;
     name: string;
+    surname: string;
     address: string;
     city: string;
 
-    constructor(RUC:string, name: string, address:string, city: string, id?: string) {
+    constructor(RUC:string, name: string, surname: string, address:string, city: string, id?: string) {
         this.RUC = RUC;
         this.name = name;
+        this.surname = surname;
         this.address = address;
         this.city = city;
         this._id = id;
@@ -16,12 +18,13 @@ export class Client {
 
 export const clientConverter = {
     fromJSON: (json: any): Client => {
-        return new Client(json.RUC, json.name, json.address, json.city, json._id);
+        return new Client(json.RUC, json.name, json.surname, json.address, json.city, json._id);
     },
     toJSON: (client: Client): any => {
         return {
             RUC: client.RUC,
             name: client.name,
+            surname: client.surname,
             address: client.address,
             city: client.city,
             _id: client._id
