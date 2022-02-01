2e4f190faa98e8d3a093c86e0a97eb635524854b
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-01 08:27:56 -0500

    Strings

---
+++ Proyecto/Codigo/mzmotos/src/lib/strings.ts
@@ -0,0 +1,29 @@
+export const HINT_USERNAME = "Usuario";
+export const HINT_PASSWORD = "Contraseña";
+export const HINT_CONFIRM_PASSWORD = "Confirmar contraseña";
+export const HINT_EMAIL = "Correo electrónico";
+export const HINT_NAME = "Nombre";
+export const HINT_SURNAME = "Apellido";
+export const HINT_PHONE = "Teléfono";
+export const HINT_ADDRESS = "Dirección";
+export const HINT_CITY = "Ciudad";
+export const HINT_CI_RUC = "CI/RUC";
+export const HINT_SEARCH = "Buscar";
+
+export const NAME = "Nombre";
+export const SURNAME = "Apellido";
+export const PHONE = "Teléfono";
+export const ADDRESS = "Dirección";
+export const CITY = "Ciudad";
+export const CI_RUC = "CI/RUC";
+export const ROLE = "Rol";
+
+export const LOGIN_NAME = "Iniciar sesión";
+export const SIGNUP_NAME = "Registrarse";
+export const RESET_PASSWORD_NAME = "¿Olvidaste la contraseña?";
+export const KEEP_LOG_NAME = "Mantener sesión iniciada";
+export const LOGIN_BUTTON_NAME = "Ingresar";
+export const SIGNUP_BUTTON_NAME = "Registrarse";
+export const EDIT_BUTTON_NAME = "Editar";
+export const DELETE_BUTTON_NAME = "Eliminar";
+export const SAVE_BUTTON_NAME = "Guardar";
